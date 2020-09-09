5e217c4da919b366427210609c16fa8ebcf74823
Hugo Brandão <hugo.h.b@hotmail.com>
2020-09-09 20:21:24 -0300

    Change to Discord.js

--- bot.js
+++ bot.js
@@ -1,39 +1,59 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+
+const prefix = '!';
+
+let n = 0;
+let km = 0;
+
+client.on('message', function(message) {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
+
+	if (command === 'antonio') {
+		message.channel.send('Você quis dizer: "Antônio"?');
+	}
+	else if (command === 'casamento') {
+		message.channel.send('Você se casaria com seu cachorro?');
+	}
+	else if (command === 'pf') {
+		message.channel.send('Paulo Fernando acabou de criar mais um buraco negro enquanto isso.');
+	}
+	else if (command === 'stonks') {
+		message.channel.send('S T O N K S');
+	}
+	else if (command === 'murtabot') {
+		message.channel.send('Tem uns comando aí pra usá');
+	}
+	else if (command === 'julio') {
+		message.channel.send(`${++n} pessoas acham que o Júlio vai esquecer que tem aula hoje.`);
+	}
+	else if (command === 'bd') {
+		message.channel.send('O tal do Lafaiete?');
+	}
+	else if (command === 'roma') {
+		message.channel.send(`Romanelli já andou ${++km}km em sua Kombi com seus cabelos ao vento.`);
+	}
+	else if (command === 'limite') {
+		message.channel.send('Fessô, isso é limite?');
+	}
+	/* if (command === "game") {
+    message.channel.send(`O jogo sendo jogado é: `);
+  }
+  if (command === "setgame") {
+    message.channel.send(`O jogo sendo jogado é: `);
+  }*/
+
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+client.on('ready', () => {
+	console.log('Murtabot is ready!.');
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+client.login(config.BOT_TOKEN);
