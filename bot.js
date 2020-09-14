76514f1fef7fae5caf20eb036b7411e321dcb672
Hugo Brandão <hugo.h.b@hotmail.com>
2020-09-14 20:55:22 -0300

    Add new Commands

--- bot.js
+++ bot.js
@@ -43,6 +43,24 @@ client.on('message', function(message) {
 	else if (command === 'limite') {
 		message.channel.send('Fessô, isso é limite?');
 	}
+	else if (command === 'pronto') {
+		message.channel.send('Estooooou pronto, capitão!');
+	}
+	else if (command === 'f') {
+		message.channel.send('Press F to pay respects');
+	}
+	else if (command === 'murta') {
+		const c = client.users.cache.get('337945189358370816');
+		message.channel.send(`${c} Invocando esférico...`);
+	}
+	else if (command === 'ueri') {
+		const c = client.users.cache.get('310459936381468672');
+		message.channel.send(`${c} O trem roda, o trem sobe, e o trem fica. É isto.`);
+	}
+	else if (command === 'jao') {
+		const c = client.users.cache.get('376869954416541707');
+		message.channel.send(`${c} "Um dia eu tava bêbado, e saí a 120 no bairro no ano novo..."`);
+	}
 	/* if (command === "game") {
     message.channel.send(`O jogo sendo jogado é: `);
   }
