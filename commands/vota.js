0c78923b95ce0d1666574b531c6cdd5ea4204c96
Hugo Brandão <hugo.h.b@hotmail.com>
2020-09-17 23:22:35 -0300

    correct vota

--- commands/vota.js
+++ commands/vota.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'vota',
     description: 'Quem é o culpado?',
     execute(message, args) {
-        message.channel.send(`Vota no const ${message.client.users.cache.get('376869954416541707')} que é o jão`);
+        message.channel.send(`Vota no ${message.client.users.cache.get('376869954416541707')} que é o jão`);
     },
 };
