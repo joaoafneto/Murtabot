fcb7c1b5b991ae9b594c165299be141c7d047db6
Hugo Brandão <hugo.h.b@hotmail.com>
2020-09-21 19:32:34 -0300

    update hugo

--- commands/hugo.js
+++ commands/hugo.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'hugo',
-    description: 'God',
+    description: 'Tony Stonks',
     execute(message, args) {
         const c = message.client.users.cache.get('450319729354211328');
-        message.channel.send(`${c} Amém`);
+        message.channel.send(`${c} Acessando portal de acesso à Bicas`);
     },
 };
