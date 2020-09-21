dd3c36852233f1dadc64cc1616a7d23d6997a05f
Hugo Brandão <hugo.h.b@hotmail.com>
2020-09-21 18:43:19 -0300

    [BUGFIX] Fix Roma

--- commands/roma.js
+++ commands/roma.js
@@ -1,8 +1,8 @@
 let km = 0;
 
 module.exports = {
-    name: 'antonio',
-    description: 'Corretor automático',
+    name: 'roma',
+    description: 'Romanelli em sua Kombi com os cabelos ao vento.',
     execute(message, args) {
         message.channel.send(`Romanelli já andou ${++km}km em sua Kombi com seus cabelos ao vento.`);
     },
