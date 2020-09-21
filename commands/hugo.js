79e19c91d08aa40b7d6a5eaa341fdbcc3cbb1027
Hugo Brandão <hugo.h.b@hotmail.com>
2020-09-21 19:46:12 -0300

    update hugo

--- commands/hugo.js
+++ commands/hugo.js
@@ -1,10 +1,10 @@
-let horas = 5;
+let distancia = Number.MAX_SAFE_INTEGER;
 
 module.exports = {
     name: 'hugo',
-    description: 'Tony Stonks',
+    description: 'Será que chega hoje?',
     execute(message, args) {
         const c = message.client.users.cache.get('450319729354211328');
-        message.channel.send(`${c} Tempo para chegar à PUC: ${++horas} horas.`);
+        message.channel.send(`${c} Hugo está a ${--distancia} anos luz da PUC. Daqui a pouco chega.`);
     },
 };
